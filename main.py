6dd47b94c5bf310435e3b955558ab7b1cb1af32a
osama <80493362+x2smartcardallinone@users.noreply.github.com>
2026-01-18 22:35:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -134,19 +134,21 @@ async def update_embed(guild):
 
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Bot activo"
 
 def run_flask():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
-    t = Thread(target=run_flask)
-    t.start()
+    Thread(target=run_flask, daemon=True).start()
+
 
 # --- Modals ---
 
